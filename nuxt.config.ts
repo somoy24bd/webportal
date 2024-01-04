fdb3d5566da7b31bbd514dca0598b90e1f3d3298
Rashed <154266056+somoy24bd@users.noreply.github.com>
2024-01-04 12:34:13 +0600

    Update nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,9 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
+  target: 'static',
+  router: {
+    base: '/webportal/'
+  },
   devtools: { enabled: true },
   modules: [
     '@nuxt/content'
